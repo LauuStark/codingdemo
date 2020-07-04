f8eb5e8f38b5ec2f2df7d8fa68664c799821329d
unknown <laura.perezolvera@gmail.com>
2020-07-04 13:11:22 -0500

    second test

---
+++ Kid_in_store.py
@@ -0,0 +1,20 @@
+candy_list = ["Palanquetas","Glorias","Ate","Mueganos","Cocada"]
+
+allowance = 5
+
+candy_cart = []
+
+
+for x in range(len(candy_list)):
+    print("[" + str(x) + "] " + candy_list[x])
+
+
+for x in range(allowance):
+    candy = input("Which candy would you like? ")
+
+    candy_cart.append(candy_list[int(candy)])
+
+for candy in candy_cart:
+    print(candy)
+
+
